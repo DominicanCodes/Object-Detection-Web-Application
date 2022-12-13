e96cb222a3252de39e8ac3f53798c0899dbca155
cardandres <65802352+cardandres@users.noreply.github.com>
2022-12-13 15:32:30 -0500

    Add clean folder method to clear images folder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 import glob
+import os
 from emailing import send_email
 
 video = cv2.VideoCapture(0)
@@ -9,6 +10,11 @@ time.sleep(1)
 first_frame = None
 status_list = []
 count = 1
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
 
 while True:
     status = 0
@@ -28,7 +34,7 @@ while True:
     # Image detection
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
-        if cv2.contourArea(contour) < 15000:
+        if cv2.contourArea(contour) < 13000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
@@ -45,8 +51,9 @@ while True:
 
     if status_list[0] == 1 and status_list[1] == 0:
             send_email(image_with_object)
+            clean_folder()
 
-    cv2.imshow("My video", frame)
+    cv2.imshow("My video", dil_frame)
 
     key = cv2.waitKey(1)
     if key == ord("q"):
