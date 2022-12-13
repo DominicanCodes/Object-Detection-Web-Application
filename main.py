8720ed0873c28803e1a5a650b19da2869a91d014
cardandres <65802352+cardandres@users.noreply.github.com>
2022-12-13 15:07:35 -0500

    Screen grab image of object entering video

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import glob
 from emailing import send_email
 
 video = cv2.VideoCapture(0)
@@ -7,30 +8,37 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
 while True:
     status = 0
     check, frame = video.read()
 
     # Image pre-processing
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray_frame_gau = cv2.GaussianBlur(gray_frame, (11, 11), 0)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (9, 9), 0)
     if first_frame is None:
         first_frame = gray_frame_gau
 
     # Image Processing
     delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
-    thresh_frame = cv2.threshold(delta_frame, 150, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.threshold(delta_frame, 160, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
     # Image detection
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
-        if cv2.contourArea(contour) < 10000:
+        if cv2.contourArea(contour) < 15000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count += 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images)/2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
