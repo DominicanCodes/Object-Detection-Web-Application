e12fb23c6e18ef2e7a8b94641900aec441756e9c
cardandres <65802352+cardandres@users.noreply.github.com>
2022-12-13 15:25:05 -0500

    Add email feature when motion is detected

--- emailing.py
+++ emailing.py
@@ -1,2 +1,28 @@
-def send_email():
-    print("Email was sent")
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+SENDER = "andrescardenas280@gmail.com"
+PASSWORD = 'avreucjhpvkjfhqa'
+RECEIVER = "andrescardenas280@gmail.com"
+
+def send_email(image_path, receiver=RECEIVER):
+    email_message = EmailMessage()
+    email_message["Subject"] = "Motion Detected"
+    email_message.set_content("Here is the object detected.")
+
+    with open(image_path, 'rb') as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", 
+                                    subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, receiver, email_message.as_string())
+    gmail.quit()
+    print("Email sent!")
+
+if __name__ == "__main__":
+    send_email(image_path="images/1.png")
